bb6ae4c65a1e0766bb5009d361e7098446e3564e
hiroki-Yim <imhk_@naver.com>
2019-03-14 11:19:45 +0900

    📪sms massage function added

---
+++ app/Console/Commands/SendSms.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use AWS;
+
+# aws를 이용하여 문자 보내기 
+# handle부분을 글이 댓글이 달릴 때 호출 (sns 수신 여부)
+
+class SendSms extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:sms {phonenum} {body}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send SMS';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $phonenum = $this->argument('phonenum');
+        $body = $this->argument('body');
+        $sns = AWS::createClient('Sns');
+        $sns->publish(['Message' => $body, 'PhoneNumber' => $phonenum]);
+    }
+}
