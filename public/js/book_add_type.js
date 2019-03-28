0e99d54f86e37a4fded209527c6909f19effca4d
sunsilver <jk05221@naver.com>
2019-03-28 09:44:30 +0900

    Revert "Revert "작품추가, 챕터""
    This reverts commit 180867e5c9f8c18fdd7a4826b9d1e789f6596acd.

--- public/js/book_add_type.js
+++ public/js/book_add_type.js
@@ -27,15 +27,25 @@ $(document).ready(function () {
 
         if (radioValue == "1") {
             hideExclude("change1");
-        } else if (radioValue == "2" || radioValue == "3") {
+            hideExclude("rent");
+        } else if (radioValue == "2") {
             hideExclude("change2");
         } else if (radioValue == "3") {
             hideExclude("change3");
+
         }
 
     });
 
+    $("input[name=radio_T]").click(function () {
 
+        var radioValue = $(this).val();
+
+        if (radioValue == "3") {
+            $('#rent').hide();
+        }
+
+    });
 
 
 });
