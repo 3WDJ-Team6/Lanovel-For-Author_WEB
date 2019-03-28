7430f27a7957d71df3be17379bd4f33b696c2a40
EUNCHAE98 <tie0510@naver.com>
2019-03-28 10:23:27 +0900

    #

--- resources/views/editor/main/book_add.blade.php
+++ resources/views/editor/main/book_add.blade.php
@@ -94,6 +94,7 @@
                                 </div>
                                 <button type="submit" class="btnSubmit">등록</button>
                                 <button type="button" class="btnSubmit" onclick="location.href='{{url('/')}}'">취소</button>
+
                     </form>
                 </div>
             </div>
