58f9dadb7e555e89fce3aa802b8de4406fd01607
sunsilver <jk05221@naver.com>
2019-03-28 09:19:44 +0900

    작품추가, 챕터

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -8,7 +8,6 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-
             <p class="copyright text-muted">Copyright &copy; Group 6 capstone project 2019</p>
         </div>
     </div>
