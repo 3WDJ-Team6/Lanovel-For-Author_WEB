72150bfa9beb0158039566db144f0c3f825a3309
tig02389 <44830851+tig02389@users.noreply.github.com>
2019-04-16 09:58:43 +0900

    Update ChatController.php

--- app/Http/Controllers/Chat/ChatController.php
+++ app/Http/Controllers/Chat/ChatController.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Str;
 
 class ChatController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
