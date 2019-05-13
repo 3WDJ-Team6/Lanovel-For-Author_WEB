b843280060ff5afa031ee0cecb9f07f2b7858622
EUNCHAE98 <tie0510@naver.com>
2019-05-13 13:42:56 +0900

    마스터에서 실수함

--- database/migrations/selected/2019_03_15_143751_create_messages_table.php
+++ database/migrations/selected/2019_03_15_143751_create_messages_table.php
@@ -16,15 +16,20 @@ class CreateMessagesTable extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('num')->comment("메시지번호");
 
+            $table->integer('num_of_work')->comment("작품번호")->unsigned();
+            $table->foreign('num_of_work')
+                ->references('num')->on('works')
+                ->onDelete('cascade');
+
             $table->integer('from_id')->comment("보낸회원번호")->unsigned();
             $table->foreign('from_id')
-                  ->references('id')->on('users')
-                  ->onDelete('cascade');
+                ->references('id')->on('users')
+                ->onDelete('cascade');
 
             $table->integer('to_id')->comment("받는회원번호")->unsigned();
             $table->foreign('to_id')
-            ->references('id')->on('users')
-            ->onDelete('cascade');
+                ->references('id')->on('users')
+                ->onDelete('cascade');
 
             $table->string('message_title')->comment("메시지제목");
             $table->text('message_content')->comment("메시지내용");
