52d43f761a21b79f601fe09b88a3f4fe74500189
sunsilver <jk05221@naver.com>
2019-05-13 21:54:47 +0900

    에디터 구린거 디자인 수정

--- resources/views/editor/main/chapter.blade.php
+++ resources/views/editor/main/chapter.blade.php
@@ -9,17 +9,20 @@
 
 </style>
 <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
+<link href="{{asset('css/templatemo_style.css')}}" rel="stylesheet">
 <script langauge="javascript">
     function popup(num) {
         var url = "/chapter_create/" + num;
         var option = "width=600, height=300, top=100"
         window.open(url, "", option);
     }
+
 </script>
 <script>
     // function receiver() {
     //     document.
     // }
+
 </script>
 @endsection
 
@@ -30,62 +33,69 @@
 @section('content')
 
 <!-- Main Content -->
-<div class="container" style="margin-top:70px;">
+<div class="container" style="margin-top:3%; margin-botttom:3%;">
     @if(Session::has('success'))
     <div class="alert alert-info">{{ Session::get('success') }}</div>
     @endif
     <!-- Material inline 1 -->
-    <div class="form-check form-check-inline" style="width:100%; align-items: center; display: flex; justify-content: center;"></div>
+    <div class="form-check form-check-inline" style="width:100%;"></div>
 
-    <div class="row">
-        <div class="col-lg-12 col-md-10 mx-auto" id="chapters_box" style="margin-top:50px; margin-bottom:50px;">
-            <div id="work_title_box" style="margin-bottom:50px;">
-                <h4 style="margin-bottom:20px;">
-                    @if(Auth::user()['roles']==2)<a href="/" style="text-decoration:none;">{{$nowWork->work_title}}</a></h4>
-                @else
-                <h4>{{$nowWork->work_title}}</h4>
-                @endif
+    <!-- <div class="row"> -->
+    <div class="col-lg-12 col-md-10 mx-auto" id="chapters_box" style="margin-top:50px; margin-bottom:50px;">
+        <div id="work_title_box" style="margin-bottom:50px;">
+            <h4 style="margin-bottom:20px;">
+                @if(Auth::user()['roles']==2)<a href="/" style="text-decoration:none;">{{$nowWork->work_title}}</a></h4>
+            @else
+            <h4>{{$nowWork->work_title}}</h4>
+            @endif
 
-                <p>{{$nowWork->introduction_of_work}}</p>
-            </div>
-            <div class="chapter" style="align-items; center; display: flex; justify-content: center;">
-            <div class="post-preview" style=" display:inline-block; margin:2%; ">
-                <h4 class="post-subtitle" style="border:2px solid #9DCFFF; border-radius:10px; width:315px; height:315px; align-items; display: flex; justify-content: center;">
-                    <a href="javascript:popup({{$num}})" target="_blank" style="display:block; color:black; text-decoration:none;">
-                    <img src="{{asset('image/plus.png')}}" alt="표지1"
-                        style="width:130px; height:130px; margin:2%; margin-top:50%; display:block;" class="img-thumbnail">챕터
-                        추가</a>
-                </h4>
+            <p>{{$nowWork->introduction_of_work}}</p>
+        </div>
+        <div class="chapter" style="align-items:center; justify-contents:center;">
+            <div class="post-preview" style=" margin-top:0; width:110px; display:inline-block; margin:0.3%">
+                <h5 class="post-subtitle" style="display:inline-block; ">
+                    <a href="javascript:popup({{$num}})" target="_blank"
+                        style="display:block; color:black; text-decoration:none;">
+                        <img src="{{asset('image/plus.png')}}" alt="표지1"
+                            style="width:70px; height:70px; margin-bottom:130%; display:block;">
+                    </a>
+                </h5>
             </div>
 
             @foreach($works as $row)
 
             @foreach($checkNum as $cn)
             @if($row->num == $cn->num)
-            <div class="post-preview" style="display: flex; justify-content: center; align-items; center; text-aligns:center; border-radius:10px; border:2px solid #9DCFFF; width:320px; height:320px; display:block; margin:2%;">
+            <div class="post-preview" style="width:230px; height:230px; display:inline-block; margin:0.3%">
                 <a href="{{url('editor/main/list')}}/{{$row['num']}}" style="text-decoration:none;">
-                    <img src="{{asset('image/note.png')}}" alt="표지1"
-                        style="width:150px; height:200px; display:block; margin-left:25%; margin-top:5%; margin-bottom:5%; box-shadow: 10px 10px 10px -5px rgba(0, 0, 0, 1);" class="img-thumbnail">
-                    <h5 class="post-subtitle" style="display:inline-block; display: flex; justify-content: center; align-items; center;">
-
+                        <img src="{{asset('image/note.png')}}" alt="표지1"
+                            style="width:110px; height:150px; display:block; margin-left:25%; margin-top:5%; margin-bottom:10%; box-shadow: 5px 5px 15px -7px rgba(0, 0, 0, 1);"
+                            class="img-thumbnail">
+                    <!-- <div class="subtitle-form" style="display:inline-block;"> -->
+                    <h6 class="post-subtitle"
+                        style="display:flex; margin-left:2%; justify-content: center; align-items; center;">
                         {{$row->subtitle}}
-                    </h5>
-                    
-                </a>
-                <p class="post-meta" style="font-size:15px; display:flex; margin-left:2%; margin:0; justify-content: center; align-items; center;">Posted by sunsilver on May
-                    5th</p>
-                    @if($cn->subsubtitle)
-                    <a href="{{url('publication')}}/{{$row['num_of_work']}}/{{$row['num']}}"
-                        style="text-decoration:none; float:right; display:block; margin-right:2%;"> 발행</a>
+                    </h6>
+                    <!-- </div> -->
 
-                    @endif
+                </a>
+
+                <p class="post-meta"
+                    style="font-size:13px; margin-bottom:0; display:flex; justify-content: center; align-items; center;">
+                    Posted by sunsilver on May
+                    5th</p>
+                @if($cn->subsubtitle)
+                <a href="{{url('publication')}}/{{$row['num_of_work']}}/{{$row['num']}}"
+                    style="text-decoration:none; font-size:12px; float:right; display:block;"> 발행</a>
+
+                @endif
             </div>
 
             @break
             @endif
             @endforeach
             @endforeach
-    </div>
+            <!-- </div> -->
             <!-- Pager -->
 
         </div>
